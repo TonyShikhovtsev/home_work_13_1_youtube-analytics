696ae4a918ddd502ccf933460ce50841c59afefc
TonyShikhovtsev <ssvsr2@gmail.com>
2023-08-16 08:28:47 +0500

    Корректировка в соотвествии с Flake8

---
+++ src/video.py
@@ -0,0 +1,14 @@
+class Video:
+    def __init__(self, video_id, title, video_link, view_count, like_count):
+        self.video_id = video_id
+        self.title = title
+        self.video_link = video_link
+        self.view_count = view_count
+        self.like_count = like_count
+
+class PLVideo(Video):
+    def __init__(self, video_id, playlist_id, title, video_link, view_count, like_count):
+        super().__init__(video_id, title, video_link, view_count, like_count)
+        self.playlist_id = playlist_id
+
+
